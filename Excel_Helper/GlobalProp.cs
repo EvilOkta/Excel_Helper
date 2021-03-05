a4c99535c241923224adc1242b74f48b09a50542
Виктор <ViktorGeraskin@gmail.com>
2021-03-05 19:13:12 +0200

    Добавлены комментарии в GlobalProp

--- Excel_Helper/GlobalProp.cs
+++ Excel_Helper/GlobalProp.cs
@@ -7,14 +7,14 @@ namespace Excel_Helper
 {
     class GlobalProp
     {
-        public static string CurrentData { get; set; }
-        public static string FilePath { get; set; }
-        public static string DirPath { get; set; }
-        public static ExcelPackage ExPack { get; set; }
-        public static ExcelWorksheets ListSheet { get; set; }
-        public static ExcelWorksheet WSheet { get; set; }
-        public static string HatRange { get; set; }
-        public static List<string> ListofFiles = new List<string>();
+        public static string CurrentData { get; set; } // Выбранные данные на форме выбора
+        public static string FilePath { get; set; } // путь к файлу
+        public static string DirPath { get; set; } // директория
+        public static ExcelPackage ExPack { get; set; } // развертка Excel
+        public static ExcelWorksheets ListSheet { get; set; } // листы в файле
+        public static ExcelWorksheet WSheet { get; set; } // активный лист
+        public static string HatRange { get; set; } // диапазон шапки
+        public static List<string> ListofFiles = new List<string>(); // список файлов в директории
 
     }
 }
